c4ca21fb2bc10859a137fb3bc48ccbfaeea92a81
devis350 <erik.turzinski@gmail.com>
2019-12-05 23:03:53 +0100

    merge day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,1 +1,25 @@
+#[aoc_generator(day1)]
+fn generate_moduls(input: &str) -> Vec<usize> {
+    let splits:Vec<&str> = input.split("/n").collect();
+    splits.iter().map(|x| str::parse(x).unwrap()).collect()
+}
+
+#[aoc(day1, part1)]
+fn fuel_from_modules(modules: &[usize]) -> usize {
+    modules.iter().map(|i|{ (i/3)-2}).sum()
+
+}
+
+#[aoc(day1, part2)]
+fn advanced_fuel_from_modules(modules: &[usize]) -> usize {
+    modules.iter().map(|&int| {
+        let mut sum = 0;
+        let mut i = (int / 3) - 2;
+        while i > 0 {
+            sum += i;
+            i = (i / 3) - 2;
+        }
+        sum
+    }).sum()
+}
 
